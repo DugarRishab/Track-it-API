ca1519d1f71bd0eaf853797a982e3b755b0d8b01
Rishab Dugar <www.anitadugar9163@gmail.com>
2022-05-16 12:14:17 +0530

    Handlled unhandelled and uncaught exception error

--- server.js
+++ server.js
@@ -16,10 +16,30 @@ mongoose.connect(DB, {// <- Using Mongoose Connection
 	useUnifiedTopology: true 
 }).then(() => {
 	console.log('DB connection established');
+}).catch((err) => {
+	console.log("DB CONNECTION FAILED");
+	console.log("ERR: ", err);
+});
+
+process.on('unCaughtException', (err) => {
+	console.log(`UNCAUGHT EXCEPTION -> ${err.name} - ${err.message}`);
+	console.log('App SHUTTING DOWN...');
+	process.exit(1); // <- Then will shut down the server.
+	
 });
 
 // Starting Server ->>
 const port = process.env.PORT || 8000;
 const server = app.listen(port, () => {
 	console.log(`App running at port`,  log.extra(`${port}`), '...');
+});
+
+// Catching unHandleled Rejections ->
+process.on('unhandledRejection', err => {
+	console.log(`UNHANDELLED REJECTION -> ${err.name} - ${err.message}`);
+	console.log(err);
+	console.log('App SHUTTING DOWN...');
+	server.close(() => {   // <- This will first terminate all requests
+		process.exit(1);   // <- Then will shut down the server.
+	});
 });
