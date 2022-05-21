f53dda23cabafe59b7e761f759555822fa3f9c6f
Rishab Dugar <www.anitadugar9163@gmail.com>
2022-05-21 15:56:56 +0530

    Updated app for v2

--- app.js
+++ app.js
@@ -6,13 +6,12 @@ const morgan = require('morgan');
 const path = require('path');
 const cors = require('cors');
 const compression = require('compression');
+const rateLimit = require('express-rate-limit');
 
 const userRouter = require('./routes/userRoutes');
-const companyRouter = require('./routes/companyRoutes');
 const projectRouter = require('./routes/projectRoutes');
 const teamRouter = require('./routes/teamRoutes');
 const taskRouter = require('./routes/taskRoutes');
-const viewRouter = require('./routes/viewRoutes');
 const log = require('./utils/colorCli');
 const AppError = require('./utils/appError');
 const errorController = require('./controllers/errorController');
@@ -22,26 +21,34 @@ const app = express();
 // MIDLEWARES ->>
 app.enable('trust proxy');
 
-app.use(cors());
+const originURL = !process.env.REMOTE ? "https://trackk-it.netlify.app" : "http://localhost:3000";
+console.log(process.env.REMOTE, originURL);
+app.use(cors({ credentials: true, origin: originURL }));
+app.options( originURL , cors());
 
 console.log(log.extra(`ENV = ${process.env.NODE_ENV}`));
-if (process.env.NODE_ENV === 'development') {
-	//console.log('MORGAN working');
-	app.use(morgan('dev')); // <- 3rd party Middleware Function
-}
+app.use(morgan('dev')); // <- 3rd party Middleware Function
+	
+const limiter = rateLimit({
+	max: 500, // max number of times per windowMS
+	windowMs: 60 * 60 * 1000,
+	message:
+        '!!! Too many requests from this IP, Please try again in 1 hour !!!',
+});
+
+app.use("/api/v2", limiter);
 
 app.use((req, res, next) => {	// <- Serves req time and cookies
 	req.requestTime = new Date().toISOString();
 	console.log(req.requestTime);
 	if (req.cookies) console.log(req.cookies);
-	
 	next();
 });
 
 app.locals.moment = require('moment');	// 3rd party library to format date in pug
 
-app.set('view engine', 'pug');
-app.set('views', path.join( __dirname, 'views'));		  
+// app.set('view engine', 'pug');
+// app.set('views', path.join( __dirname, 'views'));		  
 
 app.use(express.static(path.join(__dirname, 'public'))); 
 
@@ -50,7 +57,7 @@ app.use((req, res, next) => {
 	next();
 });
 
-app.use(express.json()); // <- Parses Json data
+app.use(express.json({ limit: '10kb' })); // <- Parses Json data
 app.use(express.urlencoded({ extended: true, limit: '10kb' })); // <- Parses URLencoded data
 app.use(cookieParser()); // <- parses cookie data
 
@@ -59,18 +66,10 @@ app.use(xss());   		  // <- Data Sanitization against xss
 
 app.use(compression());
 
-app.use('/api/v1/users', userRouter);
-app.use('/api/v1/companies', companyRouter);
-app.use('/api/v1/projects', projectRouter);
-app.use('/api/v1/teams', teamRouter);
-app.use('/api/v1/tasks', taskRouter);
-
-app.use((req, res, next) => {
-	res.setHeader("Content-Type", "text/html");
-	next();
-});
-
-app.use('/', viewRouter);
+app.use('/api/v2/users', userRouter);
+app.use('/api/v2/projects', projectRouter);
+app.use('/api/v2/teams', teamRouter);
+app.use('/api/v2/tasks', taskRouter);
 
 app.all('*', (req, res, next) => {     // <- Middleware to handle Non-existing Routes
 	next(new AppError(`Can't find ${req.originalUrl} on the server`, 404));
